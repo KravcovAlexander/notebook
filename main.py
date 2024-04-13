7e55bfa6ded882baec20e801f28e88c592259114
Alex <166308179+KravcovAlexander@users.noreply.github.com>
2024-04-14 02:14:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
 from tkinter import *
 from tkinter import simpledialog
-import re
+from tkinter.constants import END
+from tkinter.scrolledtext import ScrolledText
+from tkinter import filedialog
+
+def open_file():
+        file_path = filedialog.askopenfilename(filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")])
+        if file_path:
+            with open(file_path, 'r') as file:
+                content = file.read()
+            text_widget.delete('1.0', END)
+            text_widget.insert(END, content)
+
 
 
 def find(event=None):  
+    global text_widget
     find_text = simpledialog.askstring("поиск", "что нужно найти")
     if find_text:
-        content = text.get('1.0', END)
-        search = re.search(find_text, content)
-        if search:
-            start = text.search(find_text, '1.0', stopindex=END)
-            text.tag_add('found', start, f'{start}+{len(find_text)}c')
-            text.tag_config('found', background='yellow')
+        content = text_widget.get('1.0', END)
+        search = content.lower().find(find_text.lower())
+        if search != -1:
+            start = f"1.{search}"
+            end = f"{start}+{len(find_text)}c"
+            text_widget.tag_add('найдено', start, end)
+            text_widget.tag_config('найдено', background='yellow')
 
-def save_file(text):
-    name = simpledialog.askstring("cохранить", "Введите имя файла:")
+def save_file():
+    global text_widget
+    name = simpledialog.askstring("Сохранить", "Введите имя файла:")
     if name:
-        content = text.get("1.0", END) 
-        if len(content) >= 4:
-            with open(name, "w+") as f:
-                f.write(content)
+        content = text_widget.get("1.0", END)
+        with open(name, "w") as f:
+            f.write(content)
 
-root = Tk()
-root.title("Простой блокнот")
-root.geometry('400x250')
+def main():
+    global text_widget
+    root = Tk()
+    root.title("Простой блокнот")
+    root.geometry('400x250')
 
-text = Text(root)
-text.pack(fill=BOTH, expand=True, padx=10, pady=10)
+    text_widget = ScrolledText(root)
+    text_widget.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
-menu = Menu(root)
-file_menu = Menu(menu)
-file_menu.add_command(label='Сохранить',  command=lambda: save_file(text))
-file_menu.add_command(label="найти", command=find)
-menu.add_cascade(label='Файл', menu=file_menu)
-root.config(menu=menu)
+    menu = Menu(root)
+    file_menu = Menu(menu)
+    file_menu.add_command(label='Сохранить', command=save_file)
+    file_menu.add_command(label="Найти", command=find)
+    file_menu.add_command(label="Открыть файл", command=open_file)
+    menu.add_cascade(label='Файл', menu=file_menu)
+    root.config(menu=menu)
 
+    root.bind("<Control-s>", lambda event: save_file())
+    root.bind("<Control-f>", find)
 
-root.bind("<Control-s>", lambda event: save_file(text))
+    root.mainloop()
 
-root.bind("<Control-f>", find)
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
